cdbba1f19c92790de82fa0ff986aeaad31740877
Tony <andrianjaka.tony@gmail.com>
2026-03-26 22:11:03 +0300

    navigation: add hover effect for the items

--- src/modules/navigation/components/navigation-items.tsx
+++ src/modules/navigation/components/navigation-items.tsx
@@ -5,6 +5,9 @@ import { Link } from "next-view-transitions";
 import gsap from "gsap";
 
 import { useAnimation } from "@/hooks/use-animation";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
 
 export function NavigationItems() {
   const ITEMS = [
@@ -22,7 +25,7 @@ export function NavigationItems() {
     },
     {
       href: "/contact",
-      children: "Contact",
+      children: "Connect",
     },
   ];
 
@@ -37,7 +40,7 @@ export function NavigationItems() {
   });
 
   return (
-    <div ref={containerRef} className="flex flex-col items-center uppercase">
+    <div ref={containerRef} className="flex flex-col -space-y-2 items-center">
       {ITEMS.map(({ href, children }) => (
         <NavigationItem key={href} href={href}>
           {children}
@@ -52,10 +55,67 @@ type NavigationItemProps = {
   children: ReactNode;
 };
 function NavigationItem({ href, children }: NavigationItemProps) {
+  const id = children?.toString().toLowerCase() + "-link";
+
+  const splitMainLink = () => {
+    new SplitText(".navigation-item-link", {
+      type: "chars",
+      charsClass: "main-link-char",
+    });
+    new SplitText(".navigation-item-absolute", {
+      type: "chars",
+      charsClass: "absolute-link-char translate-y-full",
+    });
+  };
+
+  const { ref } = useAnimation(() => {
+    splitMainLink();
+  });
+
+  const handleMouseEnter = () => {
+    gsap.to(`#${id} .main-link-char`, {
+      y: "-100%",
+      ease: "power3.out",
+      duration: 0.5,
+      stagger: 0.02,
+    });
+    gsap.to(`#${id} .absolute-link-char`, {
+      y: 0,
+      ease: "power3.out",
+      duration: 0.5,
+      stagger: 0.02,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    gsap.to(`#${id} .main-link-char`, {
+      y: 0,
+      ease: "power3.out",
+      duration: 0.5,
+      stagger: 0.02,
+    });
+    gsap.to(`#${id} .absolute-link-char`, {
+      y: "100%",
+      ease: "power3.out",
+      duration: 0.5,
+      stagger: 0.02,
+    });
+  };
+
   return (
-    <div className="w-fit text-8xl tracking-tighter font-extrabold leading-[0.9] overflow-hidden">
-      <Link href={href} className="navigation-item-link block translate-y-full">
-        {children}
+    <div
+      ref={ref}
+      className="w-fit text-8xl font-extrabold uppercase leading-[0.9] overflow-hidden"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Link href={href} id={id} className="block relative">
+        <div className="navigation-item-link tracking-tighter translate-y-full overflow-hidden">
+          {children}
+        </div>
+        <div className="navigation-item-absolute absolute inset-0 tracking-tighter overflow-hidden">
+          {children}
+        </div>
       </Link>
     </div>
   );
