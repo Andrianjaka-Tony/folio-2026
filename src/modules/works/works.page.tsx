094c3f290991b5bbdc54951b54b484db440dce0d
Tony <andrianjaka.tony@gmail.com>
2026-03-26 20:40:45 +0300

    works: update works page background

--- src/modules/works/works.page.tsx
+++ src/modules/works/works.page.tsx
@@ -2,7 +2,7 @@ import { Link } from "next-view-transitions";
 
 export function WorksPage() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="bg-white h-screen w-screen flex items-center justify-center">
       <Link href="/navigation" className="text-xs uppercase tracking-tighter font-normal">
         Open Menu
       </Link>
